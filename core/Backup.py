a259c4fed6d1d2c52d18de8a21dce597d33029a1
Charles Teinturier <teintu.c@gmail.com>
2017-10-22 18:18:49 +0200

    Cleaning the code and making it more coherent

--- core/Backup.py
+++ core/Backup.py
@@ -17,40 +17,41 @@ class backup:
     def __init__(self, backupDir):
         self.__backupDir = backupDir
         self.__fsRsc = FileSystem()
+        # Create the base backup dir
         self.__fsRsc.makeDirs(self.__backupDir)
-
-    def __parseEmail(self, rawEmailAddress):
-        return email.utils.parseaddr(rawEmailAddress)[1]
 
     def __formatDate(self, rawEmailDate, outFormat = '%d/%m/%Y %H:%M:%S'):
         parsedEmailDate = email.utils.parsedate(rawEmailDate)
-        emailTimeStamp = time.mktime(parsedEmailDate)
-        d = datetime.datetime.fromtimestamp(emailTimeStamp)
+        timeStamp = time.mktime(parsedEmailDate)
+        d = datetime.datetime.fromtimestamp(timeStamp)
         return datetime.date.strftime(d, outFormat)
 
-    def __savePart(self, part, index):
-        if part.get_content_maintype() == self.__multiKeyWord:
-            return
-
+    def __getPartFileName(self, part, partIndex):
         filename = part.get_filename()
         if not filename:
             ext = mimetypes.guess_extension(part.get_content_type())
             if not ext:
                 # Use a generic bag-of-bits extension
                 ext = self.__defaultExt
-            filename = 'part-%03d%s' % (index, ext)
+            filename = 'part-%03d%s' % (partIndex, ext)
+        return filename
 
+    def __savePart(self, part, partIndex):
+        filename = self.__getPartFileName(part, partIndex)
         self.__fsRsc.writeToFile(os.path.join(self.__currentBackupEmailPath, filename), part.get_payload(decode=True))
 
     def save(self, rawEmail):
         msg = email.message_from_string(rawEmail.decode("utf-8"))
-        emailFrom = self.__parseEmail(msg['From'])
+        emailFrom = email.utils.parseaddr(msg['From'])[1]
         emailDate = self.__formatDate(msg['Date'], '%A %d %b %Y %H:%M:%S')
         self.__currentBackupEmailPath = os.path.join(self.__backupDir, emailFrom, emailDate)
         print("> %s => %s" % (emailDate, emailFrom))
 
+        partIndex = 0
         self.__fsRsc.makeDirs(self.__currentBackupEmailPath)
-        index = 0
         for part in msg.walk():
-            self.__savePart(part, index)
-            index += 1
+            if part.get_content_maintype() == self.__multiKeyWord:
+                continue
+
+            self.__savePart(part, partIndex)
+            partIndex += 1
